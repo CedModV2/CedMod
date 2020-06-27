1a9096cf09c4fc9b98a8e5b9a5abd7c1f589dbbc
ced777ric <ced777ric@cedmod.nl>
2020-06-27 19:18:26 +0200

    move all commands to the new commandsystem

---
+++ CedMod/Commands/CommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using CedMod.CedMod.INIT;
+using CedMod.Commands.Stuiter;
+using MEC;
+using RemoteAdmin;
+
+namespace CedMod.Commands
+{
+    public class CedModCommandHandler
+    {
+        public Plugin Plugin;
+        public CedModCommandHandler(Plugin plugin) => Plugin = plugin;
+
+        public static void RegisterRACommands()
+        {
+            Initializer.Logger.Info("INIT", "Registering RA commands...");
+            CommandProcessor.RemoteAdminCommandHandler.RegisterCommand(new StuiterParentCommand());
+            CommandProcessor.RemoteAdminCommandHandler.RegisterCommand(new LightsoutCommand());
+            CommandProcessor.RemoteAdminCommandHandler.RegisterCommand(new FFADisableCommand());
+            CommandProcessor.RemoteAdminCommandHandler.RegisterCommand(new AirstrikeCommand());
+        }
+        public static void RegisterConsoleCommands()
+        {
+            Initializer.Logger.Info("INIT", "Registering Console commands...");
+            GameCore.Console.singleton.ConsoleCommandHandler.RegisterCommand(new StuiterParentCommand());
+            GameCore.Console.singleton.ConsoleCommandHandler.RegisterCommand(new LightsoutCommand());
+            GameCore.Console.singleton.ConsoleCommandHandler.RegisterCommand(new FFADisableCommand());
+            GameCore.Console.singleton.ConsoleCommandHandler.RegisterCommand(new AirstrikeCommand());
+        }
+
+        public static void RegisterClientConsoleCommands()
+        {
+            Initializer.Logger.Info("INIT", "Registering Client Console commands...");
+        }
+        
+        public void OnRoundEnd()
+        {
+            Timing.KillCoroutines("LightsOut");
+        }
+    }
+}
