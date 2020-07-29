1406193620e658690ef2739405ceeb6e01390af5
ced777ric <ced777ric@cedmod.nl>
2020-07-29 03:43:23 +0200

    add ffa (incomplete still)

---
+++ CedMod/FriendlyFireAutoban.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Exiled.Events.EventArgs;
+using GameCore;
+using Hints;
+
+namespace CedMod
+{
+    public static class FriendlyFireAutoban
+    {
+        public static Dictionary<string, int> Teamkillers = new Dictionary<string, int>();
+        public static bool AdminDisabled = false;
+        public static void HandleKill(DyingEventArgs ev)
+        {
+            if (!RoundSummary.RoundInProgress() && !ConfigFile.ServerConfig.GetBool("ffa_enable") && AdminDisabled)
+                return;
+            if (ev.Killer == ev.Target)
+                return;
+            if (!IsTeakill(ev))
+                return;
+            //more to be done later:tm:
+        }
+
+        public static bool IsTeakill(DyingEventArgs ev)
+        {
+            if (ev.Killer == ev.Target)
+                return false;
+            if (ev.Killer.Team == ev.Target.Team)
+                return true;
+            switch (ev.Killer.Team)
+            {
+                case Team.CDP when ev.Target.Team == Team.CHI:
+                case Team.CHI when ev.Target.Team == Team.CDP:
+                case Team.RSC when ev.Target.Team == Team.MTF:
+                case Team.MTF when ev.Target.Team == Team.RSC:
+                case Team.MTF when ev.Target.Team == Team.CDP && ev.Target.IsCuffed && !ConfigFile.ServerConfig.GetBool("ffa_killingdisarmedclassdsallowed"):
+                case Team.CHI when ev.Target.Team == Team.RSC && ev.Target.IsCuffed && !ConfigFile.ServerConfig.GetBool("ffa_killingdisarmedscientistallowed"):
+                    return true;
+                case Team.CDP when ev.Target.Team == Team.CDP && !ConfigFile.ServerConfig.GetBool("ffa_dclassvsdclasstk", true):
+                default:
+                    return false;
+            }
+        }
+    }
+}
