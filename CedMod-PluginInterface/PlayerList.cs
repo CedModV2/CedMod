73b2e58322b4cdc4aa65f076e8c1286b3dd29e32
ced777ric <ced777ric@cedmod.nl>
2020-07-30 01:43:59 +0200

    reee

--- CedMod-PluginInterface/PlayerList.cs
+++ CedMod-PluginInterface/PlayerList.cs
@@ -75,7 +75,8 @@ namespace CedMod.PluginInterface
 								{
 								}
 								text = text + text2 + "(" + component.PlayerId + ") " + component.GetComponent<NicknameSync>().CombinedName.Replace("\n", string.Empty) + (flag4 ? "<OVRM>" : string.Empty);
-								text = $"<color={component.GetComponent<CharacterClassManager>().CurClass.GetColor().ToHex()}>" +
+								CharacterClassManager ccm = component.GetComponent<CharacterClassManager>()
+								text = $"<color={ccm.CurRole.classColor.ToHex()}>" +
 								       text + "</color>";
 							}
 							else
