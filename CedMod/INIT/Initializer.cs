d7f3bf5195001343d73e95b5eaac0981ed191728
ced777ric <ced777ric@cedmod.nl>
2020-08-14 13:59:04 +0200

    another version bump

--- CedMod/INIT/Initializer.cs
+++ CedMod/INIT/Initializer.cs
@@ -32,7 +32,7 @@ namespace CedMod.INIT
             int num = 3;
             int num2 = 1;
             int num3 = 0;
-            string text = "B";
+            string text = "R";
             return string.Format("{0}.{1}.{2}-{3}", num, num2, num3, text);
         }
         public static void DoPatching()
