e41f9fc696e637fd1a6b5da819bf54429dfe4991
ced777ric <ced777ric@cedmod.nl>
2020-08-21 16:33:32 +0200

    fix assembly info

--- CedMod-SurvivalOfTheFittest/Properties/AssemblyInfo.cs
+++ CedMod-SurvivalOfTheFittest/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CedMod_SurvivalOfTheFittest")]
+[assembly: AssemblyTitle("CedMod-SurvivalOfTheFittest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CedMod_SurvivalOfTheFittest")]
+[assembly: AssemblyCompany("CedMod Development Team")]
+[assembly: AssemblyProduct("CedMod-SurvivalOfTheFittest")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
