750716c62735e280f8371b5586296a64b725b056
ced777ric <ced777ric@cedmod.nl>
2020-11-29 13:27:55 +0100

    maingame ban patch

--- CedMod/Patches/BanPatch.cs
+++ CedMod/Patches/BanPatch.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 namespace CedMod.Patches
 {
     [HarmonyPatch(typeof(BanPlayer), nameof(BanPlayer.BanUser), new Type[] {typeof(GameObject), typeof(int), typeof(string), typeof(string),  typeof(bool)})]
-    public class BanPatch
+    public static class BanPatch
     {
-        public bool Prefix(GameObject user, int duration, string reason, string issuer, bool isGlobalBan)
+        public static bool Prefix(GameObject user, int duration, string reason, string issuer, bool isGlobalBan)
         {
             Initializer.Logger.Info("BANSYSTEM", $"MainGame ban patch: banning user. {duration} {reason} {issuer}");
             API.Ban(user, duration, issuer, reason, true);
