a3696c209404f635fe19f3fd25c385a35d3f7ca5
ced777ric <ced777ric@cedmod.nl>
2021-12-04 20:55:33 +0100

    version change

--- CedMod-QuerySystem/Plugin.cs
+++ CedMod-QuerySystem/Plugin.cs
@@ -33,7 +33,7 @@ namespace CedMod.QuerySystem
         public static string PanelUrl = "communitymanagementpanel.cedmod.nl";
         
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
-        public override Version Version { get; } = new Version(3, 0, 0);
+        public override Version Version { get; } = new Version(3, 0, 1);
 
         public override void OnDisabled()
         {
