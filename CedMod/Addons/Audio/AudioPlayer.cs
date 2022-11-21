db3c75a9e2c386038a939818890a46f1212e37ac
ced777ric <ced777ric@cedmod.nl>
2022-11-21 18:11:43 +0100

    yes

--- CedMod/Addons/Audio/AudioPlayer.cs
+++ CedMod/Addons/Audio/AudioPlayer.cs
@@ -4,6 +4,7 @@ using NVorbis;
 using PlayerRoles.PlayableScps.Scp939;
 using PluginAPI.Core;
 using UnityEngine;
+using VoiceChat;
 using VoiceChat.Codec;
 using VoiceChat.Networking;
 
@@ -12,6 +13,8 @@ namespace CedMod.Addons.Audio
     public class AudioPlayer : MonoBehaviour
     {
         public static Dictionary<ReferenceHub, AudioPlayer> AudioPlayers = new Dictionary<ReferenceHub, AudioPlayer>();
+
+        public static OpusEncoder Encoder { get; } = new OpusEncoder(VoiceChat.Codec.Enums.OpusApplicationType.Voip);
 
         public PlaybackBuffer PlaybackBuffer { get; } = new PlaybackBuffer();
 
@@ -22,9 +25,10 @@ namespace CedMod.Addons.Audio
         public float PlaybackSpeed { get; set; } = 0.1f;
         public int SampleRate { get; set; } = 480;
         public int ReadSampleRate { get; set; } = 480;
-        public float[] ReadBuffer { get; set; }
 
-        public AudioTransmitter Transmitter;
+        public float[] SendBuffer { get; set; }
+        public float[] ReadBuffer { get; set; }
+        public byte[] EncodedBuffer { get; } = new byte[512];
 
         public static AudioPlayer Get(ReferenceHub hub)
         {
@@ -34,8 +38,6 @@ namespace CedMod.Addons.Audio
             }
 
             player = hub.gameObject.AddComponent<AudioPlayer>();
-            player.Transmitter = hub.gameObject.AddComponent<AudioTransmitter>();
-            player.Transmitter.Owner = hub;
             player.Owner = hub;
 
             AudioPlayers.Add(hub, player);
@@ -47,6 +49,8 @@ namespace CedMod.Addons.Audio
         public void Play(string path)
         {
             VorbisReader = new NVorbis.VorbisReader(path);
+            Log.Info($"Playing with samplerate of {VorbisReader.SampleRate}");
+            SendBuffer = new float[VorbisReader.Channels * VorbisReader.SampleRate / 5];
             ReadBuffer = new float[VorbisReader.Channels * VorbisReader.SampleRate / 5];
             if (PlaybackCoroutine.IsValid)
                 Timing.KillCoroutines(PlaybackCoroutine);
@@ -59,13 +63,29 @@ namespace CedMod.Addons.Audio
             int cnt;
             while ((cnt = VorbisReader.ReadSamples(ReadBuffer, 0, ReadBuffer.Length)) > 0)
             {
-                Log.Info($"Doing: {cnt} - {VorbisReader.TotalTime} of {VorbisReader.TimePosition}");
-                yield return Timing.WaitForOneFrame;
+                yield return Timing.WaitForSeconds(PlaybackSpeed);
                 PlaybackBuffer.Write(ReadBuffer, ReadBuffer.Length);
             }
-
-            Transmitter.SendVoice(PlaybackBuffer);
             yield break;
+        }
+
+        public void Update()
+        {
+            if (Owner == null) return;
+
+            while (PlaybackBuffer.Length >= 2000)
+            {
+                PlaybackBuffer.ReadTo(SendBuffer, (long)480, 0L);
+                int dataLen = Encoder.Encode(SendBuffer, EncodedBuffer, 480);
+
+                foreach (var plr in ReferenceHub.AllHubs)
+                {
+                    if (plr.connectionToClient == null) continue;
+
+                    if (TestingCommand.FakeConnectionsIds.ContainsKey(plr.connectionToClient.connectionId)) continue;
+                    plr.connectionToClient.Send(new VoiceMessage(Owner, VoiceChat.VoiceChatChannel.Intercom, EncodedBuffer, dataLen, false));
+                }
+            }
         }
     }
 }
