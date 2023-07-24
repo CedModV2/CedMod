374145ec7d35e0d11069031435ea009a039523f4
ced777ric <ced777ric@cedmod.nl>
2023-07-25 00:15:44 +0200

    i hate transpilers i hate transpilers i hate transpilers i hate transpilers i hate transpilers i hate transpilers i hate transpilers i hate transpilers i hate transpilers i hate transpilers

--- CedMod/Addons/Events/Patches/SeedGenerationPatch.cs
+++ CedMod/Addons/Events/Patches/SeedGenerationPatch.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 namespace CedMod.Addons.Events.Patches
 {
     [HarmonyPatch(typeof(SeedSynchronizer), nameof(SeedSynchronizer.Start))]
-    public class SeedGenerationPatch
+    public static class SeedGenerationPatch
     {
         private static int _nextSeed = -1;
 
@@ -24,31 +24,15 @@ namespace CedMod.Addons.Events.Patches
                     _nextSeed = value;
             }
         }
-
-        public static int GetSeed()
+        
+        public static bool Prefix(SeedSynchronizer __instance)
         {
             if (NextSeed == -1)
-            {
-                var seed = ConfigFile.ServerConfig.GetInt("map_seed", -1);
-                return seed < 1 ? Random.Range(1, int.MaxValue) : seed;
-            }
-            
-            return NextSeed;
-        }
+                return true;
 
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions,
-            ILGenerator generator)
-        {
-            var newInstructions = instructions.ToList();
-            var offset = newInstructions.FindIndex(x => x.opcode == OpCodes.Bge_S) + 1;
-
-            newInstructions.RemoveRange(offset, 3);
-            newInstructions.Insert(offset,
-                new CodeInstruction(OpCodes.Call, 
-                    AccessTools.Method(typeof(SeedGenerationPatch), nameof(GetSeed))));
-
-            foreach (var code in newInstructions)
-                yield return code;
+            __instance.Network_syncSeed = NextSeed;
+            __instance._syncSeed = NextSeed;
+            return false;
         }
     }
 }
