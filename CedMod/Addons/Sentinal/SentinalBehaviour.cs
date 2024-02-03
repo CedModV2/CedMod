64f64ac56dbe6797c94b75a110b7a66d50d6e4cf
ced777ric <ced777ric@cedmod.nl>
2024-02-03 20:58:14 +0100

    better detector

--- CedMod/Addons/Sentinal/SentinalBehaviour.cs
+++ CedMod/Addons/Sentinal/SentinalBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,44 +15,34 @@ namespace CedMod.Addons.Sentinal
 {
     public class SentinalBehaviour: MonoBehaviour
     {
-        public static List<string> Ids = new List<string>();
-        public float Time = 10;
         public float AuthTimer = 1;
-        public Dictionary<uint, int> FrameCount = new Dictionary<uint, int>();
+        public Dictionary<int, Dictionary<uint, int>> FrameCount = new Dictionary<int, Dictionary<uint, int>>();
+        Dictionary<uint, Dictionary<int, int>> UserFrames = new Dictionary<uint, Dictionary<int, int>>();
         
         public void FixedUpdate()
         {
-            Time -= UnityEngine.Time.fixedDeltaTime;
-
-            if (Time <= 0)
-            {
-                Ids.Clear();
-                Time = 120;
-            }
-            
             AuthTimer -= UnityEngine.Time.fixedDeltaTime;
 
             if (AuthTimer <= 0)
             {
                 VoicePacketPacket.Tracker.Clear();
                 AuthTimer = 1;
-            }
-            
-            foreach (var pack in VoicePacketPacket.PacketsSent)
-            {
-                if (CedModMain.Singleton.Config.CedMod.ShowDebug)
-                    Log.Info($"Audioguard {pack.Key} val {pack.Value}");
-                int amount = pack.Value;
-                if (amount >= 30)
+                
+                //process framedata into easily readable data for api
+                foreach (var detectionPackets in FrameCount)
                 {
-                    FrameCount.TryAdd(pack.Key, 0);
-                    FrameCount[pack.Key]++;
-                    
-                    var plr = ReferenceHub.AllHubs.FirstOrDefault(s => s.netId == pack.Key, null);
-                    if (FrameCount[pack.Key] >= 120 && plr != null && !Ids.Contains(plr.authManager.UserId))
+                    foreach (var frameData in detectionPackets.Value)
                     {
-                        Ids.Add(plr.authManager.UserId);
-                        Log.Info($"CedMod Reporting {plr.nicknameSync.MyNick} {plr.authManager.UserId}");
+                        UserFrames.TryAdd(frameData.Key, new Dictionary<int, int>());
+                        UserFrames[frameData.Key].TryAdd(detectionPackets.Key, frameData.Value);
+                    }
+                }
+
+                foreach (var userFrames in UserFrames)
+                {
+                    var plr = ReferenceHub.AllHubs.FirstOrDefault(s => s.netId == userFrames.Key, null);
+                    if (plr != null)
+                    {
                         Task.Run(async () =>
                         {
                             using (HttpClient client = new HttpClient())
@@ -60,7 +51,7 @@ namespace CedMod.Addons.Sentinal
                                 await VerificationChallenge.AwaitVerification();
                                 try
                                 {
-                                    var response = await client.GetAsync($"http{(QuerySystem.QuerySystem.UseSSL ? "s" : "")}://{QuerySystem.QuerySystem.CurrentMaster}/Api/Sentinal/Report?key={QuerySystem.QuerySystem.QuerySystemKey}&userid={plr.authManager.UserId}&data={amount}&type=VCExploit");
+                                    var response = await client.PostAsync($"http{(QuerySystem.QuerySystem.UseSSL ? "s" : "")}://{QuerySystem.QuerySystem.CurrentMaster}/Api/Sentinal/ReportV2?key={QuerySystem.QuerySystem.QuerySystemKey}&userid={plr.authManager.UserId}&type=VCExploit", new StringContent(JsonConvert.SerializeObject(userFrames), Encoding.Default, "application/json"));
                                     if (CedModMain.Singleton.Config.QuerySystem.Debug)
                                         Log.Debug(await response.Content.ReadAsStringAsync());
                                 }
@@ -72,10 +63,21 @@ namespace CedMod.Addons.Sentinal
                         });
                     }
                 }
-                else if (FrameCount.ContainsKey(pack.Key))
-                    FrameCount.Remove(pack.Key);
+                
+                UserFrames.Clear();
+                FrameCount.Clear(); //clear array as we have reported.
             }
             
+            foreach (var pack in VoicePacketPacket.PacketsSent)
+            {
+                if (pack.Value >= 30) //keep track of all exceeding packets per frame
+                {
+                    FrameCount.TryAdd(UnityEngine.Time.frameCount, new Dictionary<uint, int>());
+                    FrameCount[UnityEngine.Time.frameCount].TryAdd(pack.Key, pack.Value);
+                }
+            }
+            
+            //clear the current list for the next frame
             VoicePacketPacket.PacketsSent.Clear();
         }
     }
