729ae60be588244b10dc20e8582c92f9185629b4
ced777ric <ced777ric@cedmod.nl>
2024-08-22 20:39:57 +0200

    AAAAAAAAAAAAAAA

--- CedMod/Addons/Sentinal/Patches/VoicePacketPacket.cs
+++ CedMod/Addons/Sentinal/Patches/VoicePacketPacket.cs
@@ -43,7 +43,7 @@ namespace CedMod.Addons.Sentinal.Patches
                 if (!OpusDecoders.ContainsKey(conn.identity.netId))
                     OpusDecoders.Add(conn.identity.netId, new OpusDecoder());
                 
-                if (Floats.ContainsKey(conn.identity.netId)) 
+                if (!Floats.ContainsKey(conn.identity.netId)) 
                     Floats[conn.identity.netId] = new float[24000];
                 
                 var len = OpusDecoders[conn.identity.netId].Decode(msg.Data, msg.DataLength,  Floats[conn.identity.netId]);
