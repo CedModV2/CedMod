9cdec1b0a1b67c24618303c200d78b0c0a6f9c50
ced777ric <ced777ric@cedmod.nl>
2025-05-26 21:33:57 +0200

    trycatch incase

--- CedMod/Addons/Sentinal/Patches/Scp939LungePatch.cs
+++ CedMod/Addons/Sentinal/Patches/Scp939LungePatch.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using CedMod.Addons.QuerySystem.WS;
 using HarmonyLib;
+using LabApi.Features.Console;
 using Mirror;
 using PlayerRoles.FirstPersonControl;
 using PlayerRoles.PlayableScps.Scp939;
@@ -15,26 +17,23 @@ namespace CedMod.Addons.Sentinal.Patches
         
         public static bool Prefix(NetworkReader reader, Scp939LungeAbility __instance)
         {
-            if (__instance.State == Scp939LungeState.Triggered)
-                return true;
-            
-            if (!LungeTime.ContainsKey(__instance.Owner.netId) || LungeTime[__instance.Owner.netId].Elapsed.TotalSeconds >= 2)
-                LungeTime[__instance.Owner.netId] = Stopwatch.StartNew();
-            else
-                return false;
-            
-            WebSocketSystem.Enqueue(new QueryCommand()
+            try
             {
-                Recipient = "PANEL",
-                Data = new Dictionary<string, string>()
-                {
-                    {"SentinalType", "SCP939LungeExploit"},
-                    {"UserId", __instance.Owner.authManager.UserId},
-                    {"State", __instance.State.ToString()},
-                    {"Pos", __instance.Owner.GetPosition().ToString()}
-                }
-            });
-            return false;
+                if (__instance.State == Scp939LungeState.Triggered)
+                    return true;
+
+                if (!LungeTime.ContainsKey(__instance.Owner.netId) || LungeTime[__instance.Owner.netId].Elapsed.TotalSeconds >= 2)
+                    LungeTime[__instance.Owner.netId] = Stopwatch.StartNew();
+                else
+                    return false;
+
+                WebSocketSystem.Enqueue(new QueryCommand() { Recipient = "PANEL", Data = new Dictionary<string, string>() { { "SentinalType", "SCP939LungeExploit" }, { "UserId", __instance.Owner.authManager.UserId }, { "State", __instance.State.ToString() }, { "Pos", __instance.Owner.GetPosition().ToString() } } });
+                return false;
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"Failed to invoke 939 exploit patch: {e.ToString()}");
+            }
         }
     }
 }
