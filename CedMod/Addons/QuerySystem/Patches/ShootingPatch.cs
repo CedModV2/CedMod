ca72cadff1aee308ae7a0df5da213c886fa8da2e
ced777ric <ced777ric@cedmod.nl>
2025-06-22 13:15:52 +0200

    tps moment

--- CedMod/Addons/QuerySystem/Patches/ShootingPatch.cs
+++ CedMod/Addons/QuerySystem/Patches/ShootingPatch.cs
@@ -173,6 +173,9 @@ namespace CedMod.Addons.QuerySystem.Patches
                     if (__instance.HasPrimaryTarget)
                     {
                         var ping = __instance.PrimaryTargetHub.authManager.InstanceMode == ClientInstanceMode.ReadyClient ? (LiteNetLib4MirrorServer.Peers[firearm.Owner.connectionToClient.connectionId].Ping * 2 / (float)1000) * 2 + 0.15f : 0.4f;
+                        if (Server.Tps / Server.MaxTps <= 0.6)
+                            ping += (float)Server.Tps / (float)Server.MaxTps * 1f;
+                        
                         using (new FpcBacktracker(__instance.PrimaryTargetHub, targetPos, Math.Min(0.4f, ping)))
                         {
 #if !EXILED
