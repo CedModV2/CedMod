977ea1721a02ced42fe417c111fea47357c65e48
ced777ric <ced777ric@cedmod.nl>
2025-09-22 01:14:11 +0200

    fix whitelist

---
+++ CedMod/Addons/QuerySystem/Patches/WhitelistPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace CedMod.Addons.QuerySystem.Patches
+{
+    [HarmonyPatch(typeof(WhiteList), nameof(WhiteList.IsOnWhitelist))]
+    public static class WhitelistPatch
+    {
+        public static bool Prefix(string userId, bool __result)
+        {
+            if (QuerySystem.UseWhitelist && QuerySystem.Whitelist.Contains(userId))
+            {
+                __result = true;
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
