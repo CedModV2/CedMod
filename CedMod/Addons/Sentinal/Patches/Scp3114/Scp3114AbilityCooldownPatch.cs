cb9977f5d073790335fe9922a4b5614afb1468ed
ced777ric <ced777ric@cedmod.nl>
2025-10-26 01:57:46 +0200

    fix (why is this client side)

--- CedMod/Addons/Sentinal/Patches/Scp3114/Scp3114AbilityCooldownPatch.cs
+++ CedMod/Addons/Sentinal/Patches/Scp3114/Scp3114AbilityCooldownPatch.cs
@@ -30,9 +30,9 @@ namespace CedMod.Addons.Sentinal.Patches.Scp3114
     [HarmonyPatch(typeof(Scp3114Strangle), nameof(Scp3114Strangle.ProcessAttackRequest))]
     public class Scp3114AbilityCooldownPatch_Attack
     {
-        public static bool Prefix(Scp3114Disguise __instance, NetworkReader reader, ref Scp3114Strangle.StrangleTarget? __result)
+        public static bool Prefix(Scp3114Strangle __instance, NetworkReader reader, ref Scp3114Strangle.StrangleTarget? __result)
         {
-            if (__instance.Cooldown.IsReady)
+            if (__instance.ClientCooldown.IsReady)
                 return true;
 
             WebSocketSystem.Enqueue(new QueryCommand()
@@ -45,6 +45,27 @@ namespace CedMod.Addons.Sentinal.Patches.Scp3114
                 }
             });
             return false;
+        }
+    }
+    
+    [HarmonyPatch(typeof(Scp3114Strangle), nameof(Scp3114Strangle.ServerWriteRpc))]
+    public class Scp3114AbilityCooldownPatch_ServerWriteRPC
+    {
+        public static bool Prefix(Scp3114Strangle __instance, NetworkWriter writer)
+        {
+            switch (__instance._rpcType)
+            {
+                case Scp3114Strangle.RpcType.TargetKilled:
+                    __instance.ClientCooldown.Trigger((double) __instance._onKillCooldown);
+                    break;
+                case Scp3114Strangle.RpcType.AttackInterrupted:
+                    __instance.ClientCooldown.Trigger((double) __instance._onInterruptedCooldown);
+                    break;
+                default:
+                    __instance.ClientCooldown.Trigger((double) __instance._onReleaseCooldown);
+                    break;
+            }
+            return true;
         }
     }
 }
